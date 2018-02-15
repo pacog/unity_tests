186562bdcb06ca9080eff32f6331dfc2de52c10c
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-02-15 21:06:52 +0100

    Add cache bust

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
 const ghPages = require('gulp-gh-pages');
+const cachebust = require('gulp-cache-bust');
 
 gulp.task('deploy', ['gh-pages-upload']);
 
 gulp.task('gh-pages-upload', function() {
-  return gulp.src('./src/**/*')
-    .pipe(ghPages());
+    'use strict';
+
+    return gulp.src('./src/**/*')
+        .pipe(cachebust({
+            type: 'timestamp'
+        }))
+        .pipe(ghPages());
 });
